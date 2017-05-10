309ce8c0735ba71b68d3096a55e9a3e4efb9af16
User <user@user>
2017-05-09 20:25:31 -0700

    add body and leg fragments; add click listeners to fragment; add state restoration across orientation changes

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.android_me.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by User on 5/9/2017.
+ */
+
+public class BodyPartFragment extends Fragment {
+}
